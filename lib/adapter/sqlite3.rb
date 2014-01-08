1d332410af194aead4e6ee5771e48722c4234518
Luke Chadwick <me@vertis.io>
2014-01-09 08:58:40 +1100

    Initial commit

---
+++ lib/adapter/sqlite3.rb
@@ -0,0 +1,7 @@
+require "adapter/sqlite3/version"
+
+module Adapter
+  module Sqlite3
+    # Your code goes here...
+  end
+end
---
+++ lib/adapter/sqlite3/version.rb
@@ -0,0 +1,5 @@
+module Adapter
+  module Sqlite3
+    VERSION = "0.0.1"
+  end
+end
